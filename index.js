0ccf8a93f6bf0fc6b6288d69717914a96b8a8ff5
Aakash Dasgupta <aakashdasgupta@Aakashs-MacBook-Pro.local>
2023-04-24 11:24:29 -0500

    Added a few MongoDB Database Schemas

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/stu-social-db');
+
+const userSchema = mongoose.Schema({});
+const postSchema = mongoose.Schema({});
+const friendSchema = mongoose.Schema({});
 
 app.route("/")
 .get((req, res) => {
